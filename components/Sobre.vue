24740a01a94b19923b55c61b962491f1e4bad687
Victor Castro <victor.kinsol@outlook>
2025-04-13 12:28:08 -0300

    foto ajustada

--- components/Sobre.vue
+++ components/Sobre.vue
@@ -2,7 +2,7 @@
   <section id="sobre" class="min-h-screen py-20 px-6 bg-amber dark:bg-teal-800 text-gray-800 dark:text-gray-200">
     <div class="container mx-auto flex flex-col md:flex-row items-center justify-center gap-10 max-w-5xl">
 
-      <div class="w-40 h-40 md:w-64 md:h-64 rounded-full overflow-hidden border-4 border-primary shadow-lg shrink-0 transform transition duration-300 hover:scale-105 hover:brightness-110 hover:shadow-xl">
+      <div class="w-40 h-40 md:w-64 md:h-64 rounded-xl overflow-hidden border-4 border-primary shadow-lg shrink-0 transform transition duration-300 hover:scale-105 hover:brightness-110 hover:shadow-xl">
         <img
           src="/me.jpeg"
           alt="Foto de Victor Castro"
